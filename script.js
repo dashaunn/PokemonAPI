e38b0d5232b73da7f6962b8e630c45b1ebe66c2d
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2023-01-13 22:05:47 -0600

    added randomize button

---
+++ script.js
@@ -0,0 +1,28 @@
+let randomBtn = document.getElementById("randomBtn");
+randomBtn.addEventListener("click", getRandomPokemon);
+
+function getRandomPokemon() {
+    // generate a random number between 1 and 807 (the number of Pokémon in the National Pokédex)
+    let randomNum = Math.floor(Math.random() * 807) + 1;
+    // fetch data for the randomly generated Pokémon
+    fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(function(response) {
+        response.json().then(function(json) {
+            // update the page with the new Pokémon's data
+            img.setAttribute("src", json.sprites.front_shiny);
+            name.innerHTML = json.name;
+            ability1.innerHTML = json.abilities[0].ability.name;
+            ability2.innerHTML = json.abilities[1].ability.name;
+            type1.innerHTML = json.types[0].type.name;
+            type2.innerHTML = json.types[1].type.name;
+        });
+    });
+}
+
+function changeImage() {
+    // toggle the image source between the front_default and front_shiny sprites
+    if (img.getAttribute("src") === json.sprites.front_default) {
+        img.setAttribute("src", json.sprites.front_shiny);
+    } else {
+        img.setAttribute("src", json.sprites.front_default);
+    }
+}
