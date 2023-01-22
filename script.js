801dd78d3be46f34af2e9c7d93118a08a270c747
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2023-01-21 23:56:39 -0500

    Revised script
    The data that is displayed should be accurate. Now the script clears all fields before displaying the next Pokémon's info.

--- script.js
+++ script.js
@@ -6,31 +6,44 @@ window.addEventListener("load", function() {
     let type2 = document.getElementById("type2");
     let img = document.getElementById("img");
     let height = document.getElementById("height");
-    let weight = this.document.getElementById("weight");
+    let weight = document.getElementById("weight");
 
     let randomBtn = document.getElementById("randomBtn");
     randomBtn.addEventListener("click", getRandomPokemon);
 
-    function getRandomPokemon() {
-        // generate a random number between 1 and 807 (the number of Pokémon in the National Pokédex)
+    let swapPhoto = document.getElementById("swapPhoto");
+    swapPhoto.addEventListener("click", changeImage);
+
+
+    async function getRandomPokemon() {
+        // Clear all fields before placing data
+        name.innerHTML = "";
+        ability1.innerHTML = "";
+        ability2.innerHTML = "";
+        type1.innerHTML = "";
+        type2.innerHTML = "";
+        height.innerHTML = "";
+        weight.innerHTML = "";
+
+        // generate a random number between 1 and 807 (the number of Pokémon in the Pokédex)
         let randomNum = Math.floor(Math.random() * 807) + 1;
         // fetch data for the randomly generated Pokémon
-        fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(function(response) {
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(function(response) {
             response.json().then(function(json) {
-                // update the page with the new Pokémon's data
-                img.setAttribute("src", json.sprites.front_shiny);
-                name.innerHTML = json.name;
-                ability1.innerHTML = json.abilities[0].ability.name;
-                ability2.innerHTML = json.abilities[1].ability.name;
-                type1.innerHTML = json.types[0].type.name;
-                type2.innerHTML = json.types[1].type.name;
-                height.innerHTML = json.height;
-                weight.innerHTML = json.weight;
+            // update the page with the new Pokémon's data
+            img.setAttribute("src", json.sprites.front_default);
+            name.innerHTML = json.name;
+            ability1.innerHTML = json.abilities[0].ability.name;
+            ability2.innerHTML = json.abilities[1].ability.name;
+            type1.innerHTML = json.types[0].type.name;
+            type2.innerHTML = json.types[1].type.name;
+            height.innerHTML = json.height;
+            weight.innerHTML = json.weight;
             });
         });
     }
 
-    // to be worked with later. this will allow a user to select another photo for the pokemon
+    // still working on this portion of the code.
     function changeImage() {
         // toggle the image source between the front_default and front_shiny sprites
         if (img.getAttribute("src") === json.sprites.front_default) {
