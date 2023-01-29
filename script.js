ce73a82f65537280455ad527c91dc06bbfee93ba
Dashaun Sutton-Harris <114096734+dashaunn@users.noreply.github.com>
2023-01-28 21:45:11 -0600

    separated left and right portions, cleaned script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-    let id = this.document.getElementById("id");
+    let id = document.getElementById("id");
     let name = document.getElementById("pokeName");
     let img = document.getElementById("img");
     let type = document.getElementById("type");
@@ -7,8 +7,8 @@ window.addEventListener("load", function() {
     let move2 = document.getElementById("move2");
     let move3 = document.getElementById("move3");
     let xp = document.getElementById("xp");
-    let height = this.document.getElementById("height");
-    let weight = this.document.getElementById("weight");
+    let height = document.getElementById("height");
+    let weight = document.getElementById("weight");
 
     let randomBtn = document.getElementById("randomBtn");
     randomBtn.addEventListener("click", getRandomPokemon);
@@ -19,12 +19,12 @@ window.addEventListener("load", function() {
 
     async function getRandomPokemon() {
 
-
         // generate a random number between 1 and 807 (the number of Pokémon in the Pokédex)
         let randomNum = Math.floor(Math.random() * 807) + 1;
         // fetch data for the randomly generated Pokémon
         let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNum}`).then(function(response) {
             response.json().then(function(json) {
+            //changeImage(json);
             // update the page with the new Pokémon's data
             id.innerHTML = json.id;
             name.innerHTML = json.name;
@@ -35,7 +35,7 @@ window.addEventListener("load", function() {
             move3.innerHTML = json.moves[2].move.name;
             xp.innerHTML = json.base_experience;
             height.innerHTML = json.height;
-            //The API
+            //Convert from metric units to imperial units
             let decimeters = json.height;
             let meters = decimeters * 0.1;
             let feet = meters * 3.2808;
@@ -52,7 +52,8 @@ window.addEventListener("load", function() {
     }
 
     // still working on this portion of the code.
-    function changeImage() {
+    function changeImage(json) {
+        console.log("changeImage called")
         // toggle the image source between the front_default and front_shiny sprites
         if (img.getAttribute("src") === json.sprites.front_default) {
             img.setAttribute("src", json.sprites.front_shiny);
